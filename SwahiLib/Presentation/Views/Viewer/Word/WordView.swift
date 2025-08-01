a85814c544fa46aa8d5b7bd00df4a80ec6ce82e1
Siro Daves <sirodevske@gmail.com>
2025-08-01 18:46:54 +0300

    minor refactors

--- SwahiLib/Presentation/Views/Viewer/Word/WordView.swift
+++ SwahiLib/Presentation/Views/Viewer/Word/WordView.swift
@@ -3,80 +3,80 @@
 //  SwahiLib
 //
 //  Created by Siro Daves on 01/08/2025.
-//
-//
-//import SwiftUI
-//
-//struct WordView: View {
-//    @StateObject private var viewModel: WordViewModel = {
-//        DiContainer.shared.resolve(WordViewModel.self)
-//    }()
-//    
-//    let word: Word
-//    
-//    @State private var showToast = false
-//
-//    var body: some View {
-//        ZStack {
-//           NavigationStack {
-//               stateContent
-//           }
-//           
-//            if showToast {
-//                let toastMessage = viewModel.isLiked
-//                    ? "\(word.title) added to your likes"
-//                    : "\(word.title) removed from your likes"
-//                
-//                ToastView(message: toastMessage)
-//                    .transition(.move(edge: .bottom).combined(with: .opacity))
-//                    .zIndex(1)
-//            }
-//       }
+
+
+import SwiftUI
+
+struct WordView: View {
+    @StateObject private var viewModel: WordViewModel = {
+        DiContainer.shared.resolve(WordViewModel.self)
+    }()
+    
+    let word: Word
+    
+    @State private var showToast = false
+
+    var body: some View {
+        ZStack {
+           NavigationStack {
+               stateContent
+           }
+           
+            if showToast {
+                let toastMessage = viewModel.isLiked
+                    ? "\(word.title) added to your likes"
+                    : "\(word.title) removed from your likes"
+                
+                ToastView(message: toastMessage)
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                    .zIndex(1)
+            }
+       }
 //        .task({viewModel.loadWord(word: word)})
-//        .onChange(of: viewModel.uiState) { newState in
-//            if case .liked = newState {
-//                showToast = true
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//                    showToast = false
-//                }
-//            }
-//        }
-//    }
-//    
-//    @ViewBuilder
-//    private var stateContent: some View {
-//        switch viewModel.uiState {
-//            case .loading:
-//                ProgressView()
-//                    .scaleEffect(5)
-//                    .tint(.primary1)
-//            
-//            case .loaded:
-//                mainContent
-//            
-//            case .liked:
-//                mainContent
-//            
+        .onChange(of: viewModel.uiState) { newState in
+            if case .liked = newState {
+                showToast = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    showToast = false
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var stateContent: some View {
+        switch viewModel.uiState {
+            case .loading:
+                ProgressView()
+                    .scaleEffect(5)
+                    .tint(.primary1)
+            
+            case .loaded:
+                mainContent
+            
+            case .liked:
+                mainContent
+            
 //            case .error(let msg):
 //                ErrorView(message: msg) {
 //                    Task { viewModel.loadSong(song: song) }
 //                }
-//                
-//            default:
-//                LoadingView()
-//        }
-//    }
-//    
-//    private var mainContent: some View {
-//        VStack(spacing: 20) {
-//            EmptyView()
-//            .fixedSize(horizontal: false, vertical: true)
-//        }
-//        .background(.accent2)
-//        .navigationTitle(viewModel.title)
-//    }
-//}
-//
+                
+            default:
+                EmptyView()
+        }
+    }
+    
+    private var mainContent: some View {
+        VStack(spacing: 20) {
+            EmptyView()
+            .fixedSize(horizontal: false, vertical: true)
+        }
+        .background(.accent2)
+        .navigationTitle(viewModel.title)
+    }
+}
+
 //#Preview{
 //    WordView(
 //        song: Song(
